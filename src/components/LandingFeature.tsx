f6833dd978494c73e42531bd0b4683faef958d10
Tawanda Kanyangarara <kliation@gmail.com>
2021-10-16 15:22:27 +0100

    change landing

--- src/components/LandingFeature.tsx
+++ src/components/LandingFeature.tsx
@@ -1,44 +1,10 @@
 
-import pic01 from '../assets/images/pic01.jpg';
+
 import pic02 from '../assets/images/pic02.jpg';
-import pic03 from '../assets/images/pic03.jpg';
-import pic04 from '../assets/images/pic04.jpg';
+
 const LandingFeature = () => {
     return (
         <div>
-            <article className="post">
-
-                <header className="major">
-                    <h2>Who we are</h2>
-                    <p>We are currently collecting clothing, sanitary ware, shoes, baby-care and cash  donations to send to vulnerable communities in Zimbabwe. Our main goal at the  moment is to raise adequate funding to send a shipping container to Zimbabwe.  We currently have a number of boxes of donations waiting to be sent. We are a  collaborative charity. Creating long-term partnerships is one of our key goals</p>
-                    <img src={pic01} alt="" />
-                    <ul className="actions special">
-                        <li><a href="/" className="button large">Full Story</a></li>
-                    </ul>
-                </header>
-                <header>
-                    <h2>Our Mission</h2>
-                    <p>To be the catalyst of change for vulnerable and disadvantaged Zimbabweans</p>
-                </header>
-
-                <header>
-                    <h2>Our Statement</h2>
-                    <p>To create a world where all Zimbabweans are supported to live purposeful and  meaningful lives.</p>
-                </header>
-
-
-            </article>
-            <article>
-                
-                
-                <div className="box alt">
-                    <div className="row gtr-50 gtr-uniform">
-                        <div className="col-4"><span className="image fit"><img src={pic02} alt="" /></span></div>
-                        <div className="col-4"><span className="image fit"><img src={pic02} alt="" /></span></div>
-                        <div className="col-4"><span className="image fit"><img src={pic02} alt="" /></span></div>
-                    </div>
-                </div>
-            </article>
             <article className="post featured">
                 <header>
                     <h2>Our Values</h2>
@@ -60,6 +26,29 @@ const LandingFeature = () => {
                 </header>
 
             </article>
+            <header>
+                <h2>Our Mission</h2>
+                <p>To be the catalyst of change for vulnerable and disadvantaged Zimbabweans</p>
+            </header>
+
+            <header>
+                <h2>Our Statement</h2>
+                <p>To create a world where all Zimbabweans are supported to live purposeful and  meaningful lives.</p>
+            </header>
+
+       
+            <article>
+
+
+                <div className="box alt">
+                    <div className="row gtr-50 gtr-uniform">
+                        <div className="col-4"><span className="image fit"><img src={pic02} alt="" /></span></div>
+                        <div className="col-4"><span className="image fit"><img src={pic02} alt="" /></span></div>
+                        <div className="col-4"><span className="image fit"><img src={pic02} alt="" /></span></div>
+                    </div>
+                </div>
+            </article>
+
 
 
 
