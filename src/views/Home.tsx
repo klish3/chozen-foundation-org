30536dfe2683f25bf45683f725ec7df212478fea
Klish3 <klish3@gmail.com>
2021-11-17 19:56:39 +0000

    folder restructure

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,10 @@
 
 
-import HomeBanner from '../shared/component/HomeBanner';
-import HomeCarousel from '../shared/component/HomeCarousel';
-import HomeFooter from '../shared/component/HomeFooter';
-import HomeGrid from '../shared/component/HomeGrid';
-import HomeGridTwo from '../shared/component/HomeGridTwo';
+import HomeBanner from '../shared/component/Home/HomeBanner';
+import HomeCarousel from '../shared/component/Home/HomeCarousel';
+import HomeFooter from '../shared/component/Home/HomeFooter';
+import HomeGrid from '../shared/component/Home/HomeGrid';
+import HomeGridTwo from '../shared/component/Home/HomeGridTwo';
 
 const Home = () => {
     return (
