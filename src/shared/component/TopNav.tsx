4ab7f8130c5ed1aea24b8191467ec7364fd71697
Klish3 <klish3@gmail.com>
2021-12-14 09:35:57 +0000

    header up date

--- src/shared/component/TopNav.tsx
+++ src/shared/component/TopNav.tsx
@@ -17,13 +17,15 @@ const TopNav = () => {
 
     return (
         <div>
-            <HomeHeaderBanner />
+            <div className='hidden sm:block'>
+                <HomeHeaderBanner />
+            </div>
             <Disclosure as="nav" className="bg-info">
                 {({ open }) => (
                     <>
                         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                             <div className="relative flex items-center justify-between h-16">
-                                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                                     {/* Mobile menu button*/}
                                     <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:text-bold focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                         <span className="sr-only">Open main menu</span>
